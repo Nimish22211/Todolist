70ed817afdaf06ad52658cc426a349283ecc7a67
Nimish22211 <83551068+Nimish22211@users.noreply.github.com>
2021-06-15 00:06:31 +0530

    added react files to github

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import Todo from './Todo'
+import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+//get data from local storage
+const getData = () => {
+  let todo = localStorage.getItem('todos');
+  if (todo) {
+    return JSON.parse(localStorage.getItem('todos'));
+  } else {
+    return []
+  }
+}
+function App() {
+  // useState([{ title: ['Add todo item'], desc: ['Add description'] }]);
+  const [todos, setTodos] = useState(getData());
+  const [inputTitle, setInputTitle] = useState('');
+  const [inputDesc, setInputDesc] = useState('');
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { title: [inputTitle], desc: [inputDesc] }]);
+    setInputTitle('');
+    setInputDesc('')
+
+  }
+  const onDelete = (id) => {
+    setTodos((todos) => {
+      return todos.filter((arrItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+  const deleteAll = () => {
+    setTodos([])
+  }
+  // to save in local storage
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
+  return (
+    <div className="app-container">
+      <div className="app">
+        <div className="head">
+          <h1>Your Todo List</h1>
+          <form className="formflex">
+            <div >
+              <FormControl>
+                <InputLabel>Write a Todo</InputLabel>
+                <Input value={inputTitle} onChange={event => setInputTitle(event.target.value)}></Input>
+              </FormControl><br /><br />
+              <FormControl>
+                <InputLabel>Write description (optional)</InputLabel>
+                <Input value={inputDesc} onChange={event => setInputDesc(event.target.value)}></Input>
+              </FormControl>
+            </div>
+
+
+            <div><Button className="addBtn" type="submit" disabled={!inputTitle} onClick={addTodo} variant="contained" color="primary">
+              Add Todo
+            </Button></div>
+          </form>
+        </div>
+        <div onClick={deleteAll} className="delete"><DeleteForeverIcon className="deleteIcon" />
+          <div className="deleteText">Delete All</div></div>
+        <ul>
+
+          {Object.keys(todos).map((key, index) => (
+
+            <Todo title={todos[key].title} desc={todos[key].desc} onDelete={onDelete} id={index} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Todo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Todo.css'
+import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
+import { List, ListItem } from '@material-ui/core'
+function Todo(props) {
+    return (
+        <div className="todo-container">
+            <List className="todo">
+                <div className="todoItem">
+                    <ListItem >
+                        <details>
+                            <summary id="summary">{props.title} </summary>
+                            {props.desc}
+                        </details>
+                    </ListItem>
+                    <DeleteForeverIcon onClick={() => { props.onDelete(props.id) }} />
+                </div>
+            </List>
+        </div>
+    )
+}
+
+export default Todo
