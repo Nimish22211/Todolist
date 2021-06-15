afa11f4736293e4d4b1384f65ba7623a3e9355d9
Nimish22211 <83551068+Nimish22211@users.noreply.github.com>
2021-06-15 13:07:47 +0530

    complete

--- src/Todo.js
+++ src/Todo.js
@@ -13,7 +13,7 @@ function Todo(props) {
                             {props.desc}
                         </details>
                     </ListItem>
-                    <DeleteForeverIcon onClick={() => { props.onDelete(props.id) }} />
+                    <DeleteForeverIcon id="delIcon" onClick={() => { props.onDelete(props.id) }} />
                 </div>
             </List>
         </div>
